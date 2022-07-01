50072f19efad1d0f63dac92a7858a80fbd3df0bf
vl-blinov <blinov.va93@gmail.com>
2022-07-01 22:59:34 +0300

    Add project

---
+++ src/main/java/ru/blinov/language/spotter/SpringLanguageSpotterApplication.java
@@ -0,0 +1,13 @@
+package ru.blinov.language.spotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLanguageSpotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLanguageSpotterApplication.class, args);
+	}
+
+}
