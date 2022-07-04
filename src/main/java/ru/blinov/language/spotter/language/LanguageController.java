86d2e17b1be4028b4186fdd77d79fe431e9c7408
vl-blinov <blinov.va93@gmail.com>
2022-07-05 00:22:04 +0300

    Add language controller

---
+++ src/main/java/ru/blinov/language/spotter/language/LanguageController.java
@@ -0,0 +1,25 @@
+package ru.blinov.language.spotter.language;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LanguageController {
+	
+	private LanguageService languageService;
+
+	@Autowired
+	public LanguageController(LanguageService languageService) {
+		this.languageService = languageService;
+	}
+	
+	@GetMapping("/languages")
+	public List<Language> findAllLanguages() {
+		return languageService.findAllLanguages();
+	}
+}
