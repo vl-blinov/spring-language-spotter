7eed812571bc337f8a64f17a30797c408ff04db3
vl-blinov <blinov.va93@gmail.com>
2022-07-05 20:07:05 +0300

    Change schema

--- src/main/resources/db/h2/schema.sql
+++ src/main/resources/db/h2/schema.sql
@@ -1,5 +1,35 @@
+CREATE TABLE IF NOT EXISTS language (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS country (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS language_country (
+	language_id INTEGER REFERENCES language(id),
+	country_id INTEGER REFERENCES country(id)
+);
+
+CREATE TABLE IF NOT EXISTS city (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR,
+	country_id INTEGER REFERENCES country(id)
+);
+
+CREATE TABLE IF NOT EXISTS education_center (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR,
+	city_id INTEGER REFERENCES city(id),
+	registration_fee VARCHAR,
+	rating INTEGER
+);
+
 CREATE TABLE IF NOT EXISTS course (
 	id SERIAL PRIMARY KEY,
+	education_center_id INTEGER REFERENCES education_center(id),
 	course_type VARCHAR,
 	students_per_class INTEGER,
 	course_duration VARCHAR,
@@ -10,39 +40,9 @@ CREATE TABLE IF NOT EXISTS course (
 
 CREATE TABLE IF NOT EXISTS accomodation (
 	id SERIAL PRIMARY KEY,
+	education_center_id INTEGER REFERENCES education_center(id),
 	accommodation_type VARCHAR,
 	food VARCHAR,
 	age_restriction VARCHAR,
 	price VARCHAR
-);
-
-CREATE TABLE IF NOT EXISTS education_center (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR,
-	course_id INTEGER REFERENCES course(id),
-	accommodation_id INTEGER REFERENCES accomodation(id),
-	registration_fee VARCHAR,
-	rating INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS city (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR,
-	education_center_id INTEGER REFERENCES education_center(id)
-);
-
-CREATE TABLE IF NOT EXISTS country (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR,
-	city_id INTEGER REFERENCES city(id)
-);
-
-CREATE TABLE IF NOT EXISTS language (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR
-);
-
-CREATE TABLE IF NOT EXISTS language_country (
-	language_id INTEGER REFERENCES language(id),
-	country_id INTEGER REFERENCES country(id)
 );
