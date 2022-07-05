3c44255557495615213214fc6c4663733efe8fa8
vl-blinov <blinov.va93@gmail.com>
2022-07-05 20:12:23 +0300

    Add city service and controller

---
+++ src/main/java/ru/blinov/language/spotter/city/CityController.java
@@ -0,0 +1,32 @@
+package ru.blinov.language.spotter.city;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CityController {
+	
+	private CityService cityService;
+	
+	@Autowired
+	public CityController(CityService cityService) {
+		this.cityService = cityService;
+	}
+	
+	@GetMapping("/{languageName}/{countryName}/cities")
+	public List<City> getAllCitiesForCountryAndLanguageToLearn(@PathVariable String languageName, @PathVariable String countryName) {
+		return cityService.findAllCitiesByCountryAndLanguageName(languageName, countryName);
+	}
+	
+	@GetMapping("/cities")
+	public List<City> getCities() {
+		return cityService.findAllCities();
+	}
+
+}
---
+++ src/main/java/ru/blinov/language/spotter/city/CityService.java
@@ -0,0 +1,30 @@
+package ru.blinov.language.spotter.city;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+@Service
+public class CityService {
+	
+	private CityRepository cityRepository;
+	
+	@Autowired
+	public CityService(CityRepository cityRepository) {
+		this.cityRepository = cityRepository;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<City> findAllCitiesByCountryAndLanguageName(String languageName, String countryName) {
+		return cityRepository.findAllByCountryAndLanguageName(StringUtils.capitalize(languageName), StringUtils.capitalize(countryName));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<City> findAllCities() {
+		return cityRepository.findAll();
+	}
+
+}
