aa81b25954365fd9f6adf66676fbfd83338b72a7
vl-blinov <blinov.va93@gmail.com>
2022-07-05 20:13:04 +0300

    Update data.sql

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO language (name) VALUES ('English');
 INSERT INTO country (name) VALUES ('Ireland');
 INSERT INTO language_country (language_id, country_id) VALUES (1, 1);
+INSERT INTO city (name, country_id) VALUES ('Dublin', 1);
