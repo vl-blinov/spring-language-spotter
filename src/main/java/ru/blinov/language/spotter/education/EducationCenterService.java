128c8af1853ab06b18ded206129b39d93f1d28bf
vl-blinov <blinov.va93@gmail.com>
2022-07-06 19:02:31 +0300

    Update education center service

--- src/main/java/ru/blinov/language/spotter/education/EducationCenterService.java
+++ src/main/java/ru/blinov/language/spotter/education/EducationCenterService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 @Service
@@ -16,6 +17,7 @@ public class EducationCenterService {
 		this.educationCenterRepository = educationCenterRepository;
 	}
 	
+	@Transactional(readOnly = true)
 	public List<EducationCenter> findAllCentersByCityAndCountryAndLanguageName(String languageName, String countryName, String cityName) {
 		return educationCenterRepository.findAllByCityAndCountryAndLanguageName(StringUtils.capitalize(languageName),
 																				StringUtils.capitalize(countryName),
