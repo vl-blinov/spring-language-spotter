c8884d4a8992e299dee8e2dc1c8efb1368cc21f4
vl-blinov <blinov.va93@gmail.com>
2022-07-06 15:36:36 +0300

    Update education center repository

--- src/main/java/ru/blinov/language/spotter/education/EducationCenterRepository.java
+++ src/main/java/ru/blinov/language/spotter/education/EducationCenterRepository.java
@@ -1,7 +1,21 @@
 package ru.blinov.language.spotter.education;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EducationCenterRepository extends JpaRepository<EducationCenter, Integer> {
+	
+	@Query(value="SELECT *"
+			  + " FROM education_center"
+			  + " LEFT JOIN city ON city.id = education_center.city_id"
+			  + " LEFT JOIN country ON country.id = city.country_id"
+			  + " LEFT JOIN language_country ON country.id = language_country.country_id"
+			  + " LEFT JOIN language ON language_country.language_id = language.id"
+			  + " WHERE language.name =:languageName"
+			  + " AND country.name =:countryName"
+			  + " AND city.name =:cityName", nativeQuery=true)
+	public List<EducationCenter> findAllByCityAndCountryAndLanguageName(String languageName, String countryName, String cityName);
 	
 }
