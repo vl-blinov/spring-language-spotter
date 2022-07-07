0c44bcb31856e35e736690951993c40374bea2c5
vl-blinov <blinov.va93@gmail.com>
2022-07-07 22:21:34 +0300

    Update data.sql

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -7,7 +7,7 @@ INSERT INTO language_country (language_id, country_id) VALUES (1, 1);
 INSERT INTO city (name, country_id) VALUES ('Dublin', 1);
 
 INSERT INTO education_center (name, city_id, registration_fee_amount, registration_fee_currency, rating)
-VALUES ('erin_school_of_english', 1, 85, '€', 4.9);
+VALUES ('Erin School Of English', 1, 85, '€', 4.9);
 
 INSERT INTO course (education_center_id, course_type, students_per_class, course_duration, class_time, lesson_duration, price_per_week_amount, price_per_week_currency)
 VALUES (1, 'General English', 15, '1 - 25 weeks', 'Monday - Friday 09:00 - 12:15', '60 minutes', 200, '€');
