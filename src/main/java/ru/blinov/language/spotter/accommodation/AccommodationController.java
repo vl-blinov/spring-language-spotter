b566d5a762ab65653fbced09c67840a8859c18df
vl-blinov <blinov.va93@gmail.com>
2022-07-15 22:03:32 +0300

    Add POST, PUT, DELETE methods to accommodation

--- src/main/java/ru/blinov/language/spotter/accommodation/AccommodationController.java
+++ src/main/java/ru/blinov/language/spotter/accommodation/AccommodationController.java
@@ -3,10 +3,16 @@ package ru.blinov.language.spotter.accommodation;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ru.blinov.language.spotter.util.StringFormatter;
 
 @RestController
 @RequestMapping("/api")
@@ -23,5 +29,36 @@ public class AccommodationController {
 	public List<Accommodation> getAllAccommodationsOfCenterOfCityOfCountry(@PathVariable String countryName, @PathVariable String cityName,
 																		   @PathVariable String centerName) {
 		return accommodationService.findAllAccommodationsByCountryAndCityAndCenter(countryName, cityName, centerName);
+	}
+	
+	@PostMapping("/{countryName}/{cityName}/{centerName}/accommodations")
+	public Accommodation addAccommodationToCenterOfCityOfCountry(@PathVariable String countryName, @PathVariable String cityName,
+			   													 @PathVariable String centerName, @RequestBody Accommodation accommodation) {
+		
+		accommodationService.saveAccommodationOfCenterOfCityOfCountry(countryName, cityName, centerName, accommodation);
+		
+		return accommodation;	
+	}
+	
+	@PutMapping("/{countryName}/{cityName}/{centerName}/accommodations")
+	public Accommodation updateAccommodationOfCenterOfCityOfCountry(@PathVariable String countryName, @PathVariable String cityName,
+			   													 	@PathVariable String centerName, @RequestBody Accommodation accommodation) {
+		
+		accommodationService.saveAccommodationOfCenterOfCityOfCountry(countryName, cityName, centerName, accommodation);
+		
+		return accommodation;	
+	}
+	
+	@DeleteMapping("/{countryName}/{cityName}/{centerName}/accommodations/{accommodationId}")
+	public String deleteAccommodationOfCenterOfCityOfCountry(@PathVariable String countryName, @PathVariable String cityName,
+			 												 @PathVariable String centerName, @PathVariable int accommodationId) {
+		
+		accommodationService.deleteAccommodationOfCenterOfCityOfCountryById(countryName, cityName, centerName, accommodationId);
+		
+		return "Accommodation with id " + accommodationId 
+			   + " in education center: " + StringFormatter.formatPathVariable(centerName) 
+			   + ", city: " + StringFormatter.formatPathVariable(cityName) 
+			   + ", country: " + StringFormatter.formatPathVariable(countryName) 
+			   + " was deleted";
 	}
 }
--- src/main/java/ru/blinov/language/spotter/accommodation/AccommodationService.java
+++ src/main/java/ru/blinov/language/spotter/accommodation/AccommodationService.java
@@ -1,6 +1,7 @@
 package ru.blinov.language.spotter.accommodation;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,50 @@ public class AccommodationService {
 				.filter(accommodation -> accommodation.getEducationCenter().getCity().getName().equals(StringFormatter.formatPathVariable(cityName)))
 				.filter(accommodation -> accommodation.getEducationCenter().getName().equals(StringFormatter.formatPathVariable(centerName)))
 				.collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public void saveAccommodationOfCenterOfCityOfCountry(String countryName, String cityName, String centerName, Accommodation accommodation) {
+
+		Optional<Accommodation> filteredAccommodation = accommodationRepository.findAll().stream()
+				.filter(a -> a.getEducationCenter().getName().equals(StringFormatter.formatPathVariable(centerName)))
+				.filter(a -> a.getEducationCenter().getCity().getName().equals(StringFormatter.formatPathVariable(cityName)))
+				.filter(a -> a.getEducationCenter().getCity().getCountry().getName().equals(StringFormatter.formatPathVariable(countryName)))
+				.findAny();
+		
+		if(filteredAccommodation.isEmpty()) {
+			throw new RuntimeException("There are no such country/city/center");
+		}
+		
+		accommodation.setEducationCenter(filteredAccommodation.get().getEducationCenter());
+		
+		accommodationRepository.save(accommodation);
+	}
+
+	public void deleteAccommodationOfCenterOfCityOfCountryById(String countryName, String cityName, String centerName, int accommodationId) {
+		
+		List<Accommodation> filteredAccommodations = accommodationRepository.findAll().stream()
+				.filter(a -> a.getEducationCenter().getName().equals(StringFormatter.formatPathVariable(centerName)))
+				.filter(a -> a.getEducationCenter().getCity().getName().equals(StringFormatter.formatPathVariable(cityName)))
+				.filter(a -> a.getEducationCenter().getCity().getCountry().getName().equals(StringFormatter.formatPathVariable(countryName)))
+				.collect(Collectors.toList());
+		
+		if(filteredAccommodations.isEmpty()) {
+			throw new RuntimeException("There are no such country/city/center");
+		}
+		
+		Optional<Accommodation> accommodation = filteredAccommodations.stream()
+				.filter(a -> a.getId() == accommodationId)
+				.findAny();
+		
+		if(accommodation.isEmpty()) {
+			throw new RuntimeException("Accommodation with id " + accommodationId 
+									   + " in education center: " + StringFormatter.formatPathVariable(centerName) 
+									   + ", city: " + StringFormatter.formatPathVariable(cityName) 
+									   + ", country: " + StringFormatter.formatPathVariable(countryName) 
+									   + " does not exist");
+		}
+		
+		accommodationRepository.deleteById(accommodationId);
 	}
 }
