1ee4528cefd131d678ed780fa6917e275cd15f5d
vl-blinov <blinov.va93@gmail.com>
2022-07-16 22:20:41 +0300

    Add POST, PUT, DELETE methods to course

--- src/main/java/ru/blinov/language/spotter/course/CourseController.java
+++ src/main/java/ru/blinov/language/spotter/course/CourseController.java
@@ -3,10 +3,16 @@ package ru.blinov.language.spotter.course;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ru.blinov.language.spotter.util.StringFormatter;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +35,37 @@ public class CourseController {
 	public List<Course> getAllCoursesOfCenterOfCityOfCountryOfLanguageToLearn(@PathVariable String languageName, @PathVariable String countryName,
 																			  @PathVariable String cityName, @PathVariable String centerName) {
 		return courseService.findAllCoursesByLanguageAndCountryAndCityAndCenter(languageName, countryName, cityName, centerName);
+	}
+	
+	@PostMapping("/{languageName}/{countryName}/{cityName}/{centerName}/courses")
+	public Course addCourseToCenterOfCityOfCountryOfLanguageToLearn(@PathVariable String languageName, @PathVariable String countryName,
+			  														@PathVariable String cityName, @PathVariable String centerName, @RequestBody Course course) {
+		
+		courseService.saveCourseOfCenterOfCityOfCountryOfLanguageToLearn(languageName, countryName, cityName, centerName, course);
+		
+		return course;
+	}
+	
+	@PutMapping("/{languageName}/{countryName}/{cityName}/{centerName}/courses")
+	public Course updateCourseOfCenterOfCityOfCountryOfLanguageToLearn(@PathVariable String languageName, @PathVariable String countryName,
+			  														   @PathVariable String cityName, @PathVariable String centerName, @RequestBody Course course) {
+		
+		courseService.saveCourseOfCenterOfCityOfCountryOfLanguageToLearn(languageName, countryName, cityName, centerName, course);
+		
+		return course;
+	}
+	
+	@DeleteMapping("/{languageName}/{countryName}/{cityName}/{centerName}/courses/{courseId}")
+	public String deleteCourseOfCenterOfCityOfCountryOfLanguageToLearn(@PathVariable String languageName, @PathVariable String countryName,
+			   														   @PathVariable String cityName, @PathVariable String centerName, 
+			   														   @PathVariable int courseId) {
+		
+		courseService.deleteCourseOfCenterOfCityOfCountryOfLanguageToLearn(languageName, countryName, cityName, centerName, courseId);
+		
+		return "Course with id " + courseId 
+			   + " in education center: " + StringFormatter.formatPathVariable(centerName) 
+			   + ", city: " + StringFormatter.formatPathVariable(cityName) 
+			   + ", country: " + StringFormatter.formatPathVariable(countryName) 
+			   + " was deleted";
 	}
 }
--- src/main/java/ru/blinov/language/spotter/course/CourseService.java
+++ src/main/java/ru/blinov/language/spotter/course/CourseService.java
@@ -1,12 +1,14 @@
 package ru.blinov.language.spotter.course;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ru.blinov.language.spotter.language.Language;
 import ru.blinov.language.spotter.util.StringFormatter;
 
 @Service
@@ -37,4 +39,71 @@ public class CourseService {
 				.filter(course -> course.getEducationCenter().getName().equals(StringFormatter.formatPathVariable(centerName)))
 				.collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public void saveCourseOfCenterOfCityOfCountryOfLanguageToLearn(String languageName, String countryName, String cityName, String centerName, Course course) {
+		
+		Optional<Course> filteredCourse = courseRepository.findAll().stream()
+				.filter(c -> c.getEducationCenter().getName().equals(StringFormatter.formatPathVariable(centerName)))
+				.filter(c -> c.getEducationCenter().getCity().getName().equals(StringFormatter.formatPathVariable(cityName)))
+				.filter(c -> c.getEducationCenter().getCity().getCountry().getName().equals(StringFormatter.formatPathVariable(countryName)))
+				.findAny();
+		
+		if(filteredCourse.isEmpty()) {
+			throw new RuntimeException("There are no such country/city/center");
+		}
+		
+		course.setEducationCenter(filteredCourse.get().getEducationCenter());
+		
+		Optional<Language> filteredLanguage = filteredCourse.get().getEducationCenter().getLanguages().stream()
+				.filter(language -> language.getName().equals(StringFormatter.formatPathVariable(languageName)))
+				.findAny();
+		
+		if(filteredLanguage.isEmpty()) {
+			throw new RuntimeException("Language with name '" + StringFormatter.formatPathVariable(languageName) 
+									   + "' is not present in education center: " + StringFormatter.formatPathVariable(centerName) 
+									   + ", city: " + StringFormatter.formatPathVariable(cityName) 
+									   + ", country: " + StringFormatter.formatPathVariable(countryName));
+		}
+
+		course.setLanguage(filteredLanguage.get());
+		
+		courseRepository.save(course);
+	}
+	
+	@Transactional
+	public void deleteCourseOfCenterOfCityOfCountryOfLanguageToLearn(String languageName, String countryName, String cityName,
+																	 String centerName, int courseId) {
+		
+		List<Course> filteredCourses = courseRepository.findAll().stream()
+				.filter(c -> c.getEducationCenter().getName().equals(StringFormatter.formatPathVariable(centerName)))
+				.filter(c -> c.getEducationCenter().getCity().getName().equals(StringFormatter.formatPathVariable(cityName)))
+				.filter(c -> c.getEducationCenter().getCity().getCountry().getName().equals(StringFormatter.formatPathVariable(countryName)))
+				.collect(Collectors.toList());
+		
+		if(filteredCourses.isEmpty()) {
+			throw new RuntimeException("There are no such country/city/center");
+		}
+		
+		if(filteredCourses.stream().filter(c -> c.getEducationCenter().hasLanguage(languageName)).findAny().isEmpty()) {
+			throw new RuntimeException("There are no language with name '" + StringFormatter.formatPathVariable(languageName) 
+									   + "' in education center: " + StringFormatter.formatPathVariable(centerName) 
+									   + ", city: " + StringFormatter.formatPathVariable(cityName)
+									   + ", country: " + StringFormatter.formatPathVariable(countryName));
+		}
+		
+		Optional<Course> course = filteredCourses.stream()
+				.filter(c -> c.getId() == courseId)
+				.findAny();
+		
+		if(course.isEmpty()) {
+			throw new RuntimeException("Course with id " + courseId 
+									   + " in education center: " + StringFormatter.formatPathVariable(centerName) 
+									   + ", city: " + StringFormatter.formatPathVariable(cityName) 
+									   + ", country: " + StringFormatter.formatPathVariable(countryName) 
+									   + " does not exist");
+		}
+		
+		courseRepository.deleteById(courseId);
+	}	
 }
