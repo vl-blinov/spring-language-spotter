fb531f8a3e8df2e155e7f4998eb2eb3b3d4af9e9
vl-blinov <blinov.va93@gmail.com>
2022-08-09 18:41:56 +0300

    Add tests to RequestValidatorTest.java

--- src/test/java/ru/blinov/language/spotter/validator/RequestValidatorTest.java
+++ src/test/java/ru/blinov/language/spotter/validator/RequestValidatorTest.java
@@ -8,15 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import ru.blinov.language.spotter.enums.RequestUrlMessage;
 import ru.blinov.language.spotter.exception.RequestUrlException;
-import ru.blinov.language.spotter.util.StringFormatter;
 
 @SpringBootTest
 @Testcontainers
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class RequestValidatorTest {
 	
 	@Autowired
@@ -39,18 +42,222 @@ public class RequestValidatorTest {
 		String languageNamePathVariable = "english";
 
 		//Assert
-		assertDoesNotThrow(() -> sut.checkUrlPathVariables(languageNamePathVariable));
+		assertDoesNotThrow(
+				() -> sut.checkUrlPathVariables(languageNamePathVariable));
 	}
 	
 	@Test
-	public void While_checking_language_should_throw_requestUrlException_with_message_is_not_found() {
+	public void While_checking_language_should_throw_requestUrlException_LANGUAGE_NOT_FOUND() {
 		
 		//Arrange
 		String languageNamePathVariable = "englishh";
 		
-		String message = "Language with name '" + StringFormatter.formatPathVariable(languageNamePathVariable) + "' is not found";
+		String message = RequestUrlMessage.LANGUAGE_NOT_FOUND.getMessage();
 
 		//Assert
-		assertThrows(message, RequestUrlException.class, () -> sut.checkUrlPathVariables(languageNamePathVariable));
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable));
 	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_should_not_throw_any_exception() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		String countryNamePathVariable = "ireland";
+
+		//Assert
+		assertDoesNotThrow(
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable));
+	}
+	
+	@Test
+	public void While_checking_language_and_country_should_throw_requestUrlException_COUNTRY_NOT_FOUND() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		
+		String countryNamePathVariable = "irelandd";
+		
+		String message = RequestUrlMessage.COUNTRY_NOT_FOUND.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable));
+	}
+	
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_should_throw_requestUrlException_COUNTRY_LANGUAGE_DISCR() {
+		
+		//Arrange
+		String languageNamePathVariable = "french";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String message = RequestUrlMessage.COUNTRY_LANGUAGE_DISCR.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_should_not_throw_any_exception() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String cityNamePathVariable = "dublin";
+
+		//Assert
+		assertDoesNotThrow(
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_should_throw_requestUrlException_CITY_NOT_FOUND() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String cityNamePathVariable = "dublinn";
+		
+		String message = RequestUrlMessage.CITY_NOT_FOUND.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_should_throw_requestUrlException_CITY_COUNTRY_DISCR() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		
+		String countryNamePathVariable = "canada";
+		
+		String cityNamePathVariable = "dublin";
+		
+		String message = RequestUrlMessage.CITY_COUNTRY_DISCR.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_should_throw_requestUrlException_CITY_LANGUAGE_DISCR() {
+		
+		//Arrange
+		String languageNamePathVariable = "irish";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String cityNamePathVariable = "cork";
+		
+		String message = RequestUrlMessage.CITY_LANGUAGE_DISCR.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_and_center_should_not_throw_any_exception() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String cityNamePathVariable = "dublin";
+		
+		String centerNamePathVariable = "erin_school_of_english";
+
+		//Assert
+		assertDoesNotThrow(
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable, centerNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_and_center_should_throw_requestUrlException_EDUCATION_CENTER_NOT_FOUND() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String cityNamePathVariable = "dublin";
+		
+		String centerNamePathVariable = "erin_school_of_englishh";
+		
+		String message = RequestUrlMessage.EDUCATION_CENTER_NOT_FOUND.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable, centerNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_and_center_should_throw_requestUrlException_EDUCATION_CENTER_CITY_DISCR() {
+		
+		//Arrange
+		String languageNamePathVariable = "english";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String cityNamePathVariable = "dublin";
+		
+		String centerNamePathVariable = "cork_english_college";
+		
+		String message = RequestUrlMessage.EDUCATION_CENTER_CITY_DISCR.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable, centerNamePathVariable));
+	}
+	
+	@Test
+	@Transactional
+	public void While_checking_language_and_country_and_city_and_center_should_throw_requestUrlException_EDUCATION_CENTER_LANGUAGE_DISCR() {
+		
+		//Arrange
+		String languageNamePathVariable = "irish";
+		
+		String countryNamePathVariable = "ireland";
+		
+		String cityNamePathVariable = "dublin";
+		
+		String centerNamePathVariable = "erin_school_of_english";
+		
+		String message = RequestUrlMessage.EDUCATION_CENTER_LANGUAGE_DISCR.getMessage();
+
+		//Assert
+		assertThrows(message,
+				RequestUrlException.class,
+				() -> sut.checkUrlPathVariables(languageNamePathVariable, countryNamePathVariable, cityNamePathVariable, centerNamePathVariable));
+	}	
 }
