6cc26b51d936d51c3f2c19e35f88cd24f7550310
vl-blinov <blinov.va93@gmail.com>
2022-08-17 23:04:54 +0300

    Add Springdoc openAPI UI

--- src/main/java/ru/blinov/language/spotter/SpringLanguageSpotterApplication.java
+++ src/main/java/ru/blinov/language/spotter/SpringLanguageSpotterApplication.java
@@ -3,7 +3,11 @@ package ru.blinov.language.spotter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Language Spotter REST API", version = "1.0.0"))
 public class SpringLanguageSpotterApplication {
 
 	public static void main(String[] args) {
