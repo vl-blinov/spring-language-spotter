e62687bd0aa9dcd44f17dee75cc7781a9b47e8a9
vl-blinov <blinov.va93@gmail.com>
2022-08-19 15:09:10 +0300

    Add description of application on Swagger UI

--- src/main/java/ru/blinov/language/spotter/SpringLanguageSpotterApplication.java
+++ src/main/java/ru/blinov/language/spotter/SpringLanguageSpotterApplication.java
@@ -7,7 +7,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Language Spotter REST API", version = "1.0.0"))
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Language Spotter REST API", version = "1.0.0",
+				description = "The panel below displays documentation all endpoints, parameters and messages available to the Language Spotter REST API."
+		)
+)
 public class SpringLanguageSpotterApplication {
 
 	public static void main(String[] args) {
